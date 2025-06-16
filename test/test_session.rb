c706ff362385075b5d4f632aa4e2c359a494a3da
Jeff McFadden <jeff@mcfadden.io>
2025-06-15 17:03:45 -0700

    Bring back a test for the session.

--- test/test_session.rb
+++ test/test_session.rb
@@ -2,39 +2,12 @@ require_relative "test_helper"
 require 'tmpdir'
 
 class SessionTest < TLDR
-  # def test_initialize_sets_base_path_and_run_tests_cmd
-  #   base = "/path/to/project"
-  #   config = Genie::SessionConfig.new(base_path: base, run_tests_cmd: "ls")
-  #   session = Genie::Session.new(config: config)
-  #
-  #   assert_equal base, session.base_path
-  #   assert_equal "ls", session.run_tests_cmd
-  # end
-  #
-  # def test_base_path_is_read_only
-  #   config = Genie::SessionConfig.new(base_path: "/foo", run_tests_cmd: "ls")
-  #   session = Genie::Session.new(config: config)
-  #   assert_raises(NoMethodError) do
-  #     session.base_path = "/bar"
-  #   end
-  # end
-  #
-  # def test_missing_config_raises
-  #   assert_raises(ArgumentError) do
-  #     Genie::Session.new
-  #   end
-  # end
-  #
-  # def test_run_tests_cmd_from_config
-  #   Dir.mktmpdir do |dir|
-  #     config_hash = {'run_tests_cmd' => 'foobar'}
-  #
-  #     tmp_config_file_path = File.join(dir, 'genie.yml')
-  #
-  #     File.write(tmp_config_file_path, config_hash.to_yaml)
-  #     config = Genie::SessionConfig.from_file(tmp_config_file_path)
-  #     session = Genie::Session.new(config: config)
-  #     assert_equal 'foobar', session.run_tests_cmd
-  #   end
-  # end
+
+  def test_session_does_not_blow_up
+    config = Genie::SessionConfig.default
+    session = Genie::Session.new(config: config)
+
+    refute_equal nil, session
+  end
+
 end
